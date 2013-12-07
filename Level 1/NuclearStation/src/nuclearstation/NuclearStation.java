dba4b6ed19beba13f77b10bf15ee456f95515c50
Artem <mmm@mail.mail>
2013-12-07 19:02:53 +0500

    Added: NuclearStation initial project

---
+++ Level 1/NuclearStation/src/nuclearstation/NuclearStation.java
@@ -0,0 +1,9 @@
+package nuclearstation;
+
+public class NuclearStation {
+
+    public static void main(String[] args) {
+        System.out.println("Nuclear station embedded firmware 1.0");
+    }
+    
+}
