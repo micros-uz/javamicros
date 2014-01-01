5fbcac33b6420d13e96465ab9e491b09ee33097e
micros-uz <www@mail.js>
2014-01-01 11:26:05 +0500

    Added: Home Controller

---
+++ eStore/src/main/java/uz/micros/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package uz.micros.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Heluulo world!");
+        return "hello";
+    }
+}
