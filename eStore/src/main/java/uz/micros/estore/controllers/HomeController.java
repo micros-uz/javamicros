d472548e72aa0459a2971ff0b11c649fc595f23c
Artem <mmm@mail.mail>
2014-01-01 16:24:15 +0500

    Added: Thymeleaf integration!!!!!!!!!

--- eStore/src/main/java/uz/micros/estore/controllers/HomeController.java
+++ eStore/src/main/java/uz/micros/estore/controllers/HomeController.java
@@ -4,13 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
-        model.addAttribute("message", "Hi, eStore!");
-        return "hello";
+        model.addAttribute("message", "Hi,ww eStore!");
+        Date curDate =  new Date();
+        model.addAttribute("curDate", curDate.toString());
+
+        return "home";
     }
 }
