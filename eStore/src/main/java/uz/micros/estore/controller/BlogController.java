5befa9a545d4bb82edbf62a0383f23cdb57707a9
micros-uz <www@mail.js>
2014-01-04 19:19:38 +0500

    Added: Blog entity + some renamings

--- eStore/src/main/java/uz/micros/estore/controller/BlogController.java
+++ eStore/src/main/java/uz/micros/estore/controller/BlogController.java
@@ -1,23 +1,21 @@
-package uz.micros.estore.controllers;
+package uz.micros.estore.controller;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.util.Date;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/blog")
+public class BlogController {
     @RequestMapping(method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
+    public String blogIndex(ModelMap model) {
         model.addAttribute("message", "Hi,ww eStore!");
         Date curDate =  new Date();
         model.addAttribute("curDate", curDate.toString());
 
-        return "home";
+        return "blog";
     }
 }
--- eStore/src/main/java/uz/micros/estore/controller/HomeController.java
+++ eStore/src/main/java/uz/micros/estore/controller/HomeController.java
@@ -1,21 +1,20 @@
-package uz.micros.estore.controllers;
+package uz.micros.estore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.util.Date;
 
 @Controller
-@RequestMapping("/blog")
-public class BlogController {
+@RequestMapping("/")
+public class HomeController {
     @RequestMapping(method = RequestMethod.GET)
-    public String blogIndex(ModelMap model) {
+    public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hi,ww eStore!");
         Date curDate =  new Date();
         model.addAttribute("curDate", curDate.toString());
 
-        return "blogIndex";
+        return "home";
     }
 }
