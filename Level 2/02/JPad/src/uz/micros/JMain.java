7c101a8df8a4fc12c7b815dd3b5b7b3364cc1b03
micros <www@www.uz>
2014-02-26 20:05:45 +0500

    Added: InitiaL jpAD APP

---
+++ Level 2/02/JPad/src/uz/micros/JMain.java
@@ -0,0 +1,17 @@
+package uz.micros;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JMain extends JFrame {
+
+    public JMain() {
+        setSize(800, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void display() {
+        setVisible(true);
+    }
+}
