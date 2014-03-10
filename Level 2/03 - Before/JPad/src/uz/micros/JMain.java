58057ec82bead1d72f0cf9cf86c01be6577f9d50
Artem <mmm@mail.mail>
2014-03-10 12:34:08 +0500

    Added: TreeView example

--- Level 2/03 - Before/JPad/src/uz/micros/JMain.java
+++ Level 2/03 - Before/JPad/src/uz/micros/JMain.java
@@ -1,7 +1,13 @@
 package uz.micros;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.io.*;
@@ -14,6 +20,10 @@ public class JMain extends JFrame implements CommandPerformer {
 
     private JTextArea textArea;
     private File curFile;
+    private boolean onTop;
+    private JTree tree;
+    private DefaultTreeModel treeModel;
+    private FileSystemView fileSystemView;
 
     public JMain() {
         setSize(800, 500);
@@ -24,14 +34,99 @@ public class JMain extends JFrame implements CommandPerformer {
         createMenu();
         createToolBar();
         createTextArea();
+        createTree();
+        createSplitter();
+    }
+
+    private void createSplitter() {
+        JScrollPane treeScroll = new JScrollPane(tree);
+        JScrollPane textScroll = new JScrollPane(textArea);
+
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                treeScroll, textScroll);
+
+        getContentPane().add(split, BorderLayout.CENTER);
+    }
+
+    private void createTree() {
+        fileSystemView = FileSystemView.getFileSystemView();
+
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+        treeModel = new DefaultTreeModel(root);
+
+        TreeSelectionListener treeSelectionListener = new TreeSelectionListener() {
+            public void valueChanged(TreeSelectionEvent tse){
+                DefaultMutableTreeNode node =
+                        (DefaultMutableTreeNode)tse.getPath().getLastPathComponent();
+                showChildren(node);
+            }
+        };
+
+        // show the file system roots.
+        File[] roots = fileSystemView.getRoots();
+        for (File fileSystemRoot : roots) {
+            DefaultMutableTreeNode node = new DefaultMutableTreeNode(fileSystemRoot);
+            root.add( node );
+
+            File[] files = fileSystemView.getFiles(fileSystemRoot, true);
+            for (File file : files) {
+                    node.add(new DefaultMutableTreeNode(file));
+            }
+        }
+
+        tree = new JTree(treeModel);
+        tree.setRootVisible(false);
+        tree.addTreeSelectionListener(treeSelectionListener);
+        tree.setCellRenderer(new FileTreeCellRenderer());
+        tree.expandRow(0);
+
+    }
+
+    /** Add the files that are contained within the directory of this node.
+     Thanks to Hovercraft Full Of Eels. */
+    private void showChildren(final DefaultMutableTreeNode node) {
+        tree.setEnabled(false);
+        //progressBar.setVisible(true);
+        //progressBar.setIndeterminate(true);
+
+        SwingWorker<Void, File> worker = new SwingWorker<Void, File>() {
+            @Override
+            public Void doInBackground() {
+                File file = (File) node.getUserObject();
+                if (file.isDirectory()) {
+                    File[] files = fileSystemView.getFiles(file, true); //!!
+                    if (node.isLeaf()) {
+                        for (File child : files) {
+                            if (child.isDirectory()) {
+                                publish(child);
+                            }
+                        }
+                    }
+                    //setTableData(files);
+                }
+                return null;
+            }
+
+            @Override
+            protected void process(java.util.List<File> chunks) {
+                for (File child : chunks) {
+                    node.add(new DefaultMutableTreeNode(child));
+                }
+            }
+
+            @Override
+            protected void done() {
+                //progressBar.setIndeterminate(false);
+               // progressBar.setVisible(false);
+                tree.setEnabled(true);
+            }
+        };
+        worker.execute();
     }
 
     private void createTextArea() {
         textArea = new JTextArea();
         textArea.setMargin(new Insets(4, 4, 4, 4));
-        JScrollPane scroll = new JScrollPane(textArea);
-
-        getContentPane().add(scroll, BorderLayout.CENTER);
     }
 
     private Action createAction(JCommands cmd){
@@ -104,6 +199,50 @@ public class JMain extends JFrame implements CommandPerformer {
         mnFile.add(miSave);
         mnFile.addSeparator();
         mnFile.add(miExit);
+
+        JCheckBoxMenuItem miOnTop = createMenuItem2(new AbstractAction("Always On Top") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JCheckBoxMenuItem c = (JCheckBoxMenuItem)e.getSource();
+                onTop = !onTop;
+                setAlwaysOnTop(onTop);
+                c.setSelected(onTop);
+            }
+        }, 't', 0);
+
+        JCheckBoxMenuItem miTree = createMenuItem2(new AbstractAction("Show Tree") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //split.setOneTouchExpandable(true);
+               // split.setDividerLocation(0.0);
+                ProcessBuilder pb = new ProcessBuilder("cmd.exe");
+                Runtime rt = Runtime.getRuntime();
+
+                try {
+                    rt.exec(new String[]{"cmd.exe", "/c", "start"});
+                    //pb.start();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }, 'r', 0);
+
+        // http://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html#ToolBarDemo
+
+        // http://docs.oracle.com/javase/tutorialJWS/samples/uiswing/DynamicTreeDemoProject/DynamicTreeDemo.jnlp
+
+        mnView.add(miOnTop);
+        mnView.add(miTree);
+    }
+
+    private JCheckBoxMenuItem createMenuItem2(Action a, char m, int key) {
+        JCheckBoxMenuItem res = new JCheckBoxMenuItem(a);
+        res.setMnemonic(m);
+
+        if (key > 0)
+            res.setAccelerator(KeyStroke.getKeyStroke(key, InputEvent.CTRL_MASK));
+
+        return res;
     }
 
     private JMenuItem createMenuItem(Action a, char m, int key) {
@@ -130,9 +269,11 @@ public class JMain extends JFrame implements CommandPerformer {
         }
     }
 
+    private boolean d;
     private void handleNew(){
-        if (JOptionPane.showConfirmDialog(null, "Sure?", "Question", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE) == JOptionPane.YES_OPTION)
-        JOptionPane.showMessageDialog(null, "My Goodness, this is so concise");
+        if (JOptionPane.showConfirmDialog(this, "Sure?", "Question", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE) == JOptionPane.YES_OPTION)
+            setAlwaysOnTop(d = !d);
+        //JOptionPane.showMessageDialog(null, "My Goodness, this is so concise");
     }
 
     private void handleOpen(){
