cd072d29a997ae4643f97754f1173b8c927cdeac
micros-uz <www@mail.js>
2014-03-22 22:54:55 +0500

    Added: Elementary Spring XML project with one bean

--- Level 3/SpringFromScratch/my-app/src/main/java/uz/micros/app/MainApp.java
+++ Level 3/SpringFromScratch/my-app/src/main/java/uz/micros/app/MainApp.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
     public static void main(String[] args) {
         ApplicationContext context =
-                new ClassPathXmlApplicationContext("classpath:uz/micros/Beans.xml");
+                new ClassPathXmlApplicationContext("/Beans.xml");
 
         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 
